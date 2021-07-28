6c4352302a31fa20f9ff1983084ef070749e641f
Patryk <beslerpatryk@gmail.com>
2021-07-28 14:44:51 +0800

    feat: add mobile design and js script for menu

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const menu = document.getElementById('menu')
+const icon = document.getElementById('icon')
+const arrow = document.getElementById('arrow')
+const header = document.getElementById('header')
+
+menu.style.display = 'none'
+
+icon.addEventListener('click', ()=> {
+    if(menu.style.display === 'none'){
+        menu.style.display = 'flex'
+        arrow.style.display = 'none'
+        header.style.display = 'none'
+    } else {
+        menu.style.display = 'none'
+        arrow.style.display = 'block'
+        header.style.display = 'block'
+    }
+    
+
+})
+
